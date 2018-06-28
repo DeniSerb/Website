ad794a24a82a5aaaeb2ffc402383fbaed95edb63
DeniSerb <Dondo788@gmail.com>
2018-06-28 20:49:10 +0300

    add basik nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import './Assets/css/default.min.css';
+import Header from './components/headerComponent/header.js';
+import Footer from './components/footerComponent/footer.js';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         
+        <Header />
+        <Footer />
+
       </div>
     );
   }
