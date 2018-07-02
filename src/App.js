7f35829bf34e52ec51c82327be1dd57756631c69
DeniSerb <Dondo788@gmail.com>
2018-07-02 17:47:27 +0300

    add loader

--- src/App.js
+++ src/App.js
@@ -25,6 +25,23 @@ import                           './Assets/css/default.min.css';
 
 
 class App extends Component {
+
+  authenticate(){
+    return new Promise(resolve => setTimeout(resolve, 2000))
+  }
+
+  componentDidMount(){
+    this.authenticate().then(() => {
+      const ele = document.getElementById('ipl-progress-indicator')
+      if(ele){
+        ele.classList.add('available')
+        setTimeout(() => {
+          ele.outerHTML = ''
+        }, 2000)
+      }
+    })
+  }
+
   render() {
     return (
       <Router>
