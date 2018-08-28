9db04a1789075ac816a3faac1be6091ddf77576d
alextargonski <targonsky97@gmail.com>
2018-08-28 18:39:26 +0300

    started new template

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeHeader = () => (
+  <div className="home-header">
+    <div className="bordered">
+      <h3>Тепло Вашего дома – это спокойствие Вашей души</h3>
+    </div>
+  </div>
+)
+
+export default HomeHeader;
---
+++ src/components/services.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import {
+  Container,
+  Row,
+  Col}                      from 'mdbreact';
+
+class HomeHeader extends Component {
+  render() {
+    return (
+      <div className="services-wrapper">
+        <Container>
+          <section className="text-center my-5">
+            <h2 className="h1-responsive font-weight-bold my-5">Что такое пеноизол?</h2>
+            <p className="lead grey-text w-responsive mx-auto mb-5">
+              Пеноизол – универсальный теплоизоляционный материал, который используется для утепления жилых и промышленных объектов.
+              Достоинствами пеноизола, которые выделяют его среди других видов теплоизоляционных материалов
+              являются высокие тепло и звукоизоляционные свойства, долговечность, стойкость к температурным
+              колебаниям и повышению влажности, пожаробезопасность.
+            </p>
+            <Row>
+              <Col md="6">
+                <div className="services-item">
+                  <img src="./wall.jpg" width="100%" alt="wall" />
+                  <h2>Утепление стен</h2>
+                  <p className="grey-text mb-md-0 mb-5">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Reprehenderit maiores aperiam minima assumenda deleniti hic.
+                  </p>
+                  <span className="read-more">
+                    Подробнее
+                  </span>
+                </div>
+              </Col>
+              <Col md="6">
+                <div className="services-item">
+                  <img src="./roof.jpg" width="100%" alt="roof" />
+                  <h2>Утепление крыши</h2>
+                  <p className="grey-text mb-md-0 mb-5">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Reprehenderit maiores aperiam minima assumenda deleniti hic.
+                  </p>
+                  <span className="read-more">
+                    Подробнее
+                  </span>
+                </div>
+              </Col>
+              <Col md="6">
+                <div className="services-item">
+                  <img src="./floor.jpg" width="100%" alt="floor" />
+                  <h2>Утепление полов</h2>
+                  <p className="grey-text mb-md-0 mb-5">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Reprehenderit maiores aperiam minima assumenda deleniti hic.
+                  </p>
+                  <span className="read-more">
+                    Подробнее
+                  </span>
+                </div>
+              </Col>
+              <Col md="6">
+                <div className="services-item">
+                  <img src="./garage.jpg" width="100%" alt="garage" />
+                  <h2>Утепление ангаров</h2>
+                  <p className="grey-text mb-md-0 mb-5">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Reprehenderit maiores aperiam minima assumenda deleniti hic.
+                  </p>
+                  <span className="read-more">
+                    Подробнее
+                  </span>
+                </div>
+              </Col>
+            </Row>
+          </section>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default HomeHeader;
