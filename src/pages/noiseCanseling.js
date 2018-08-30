7e74c18dfc7e0538f39c5fbcb53589c83ce61ed0
DeniSerb <Dondo788@gmail.com>
2018-08-30 18:21:01 +0300

    add routes

---
+++ src/pages/noiseCanseling.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NoiseCanseling = () => (
+  <div>
+   <h1>Noise Canseling</h1>
+   <p>Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+  </div>
+);
+
+export default NoiseCanseling;
